0e5eb9cab05aeaaa7c5e7498cf68b9139df8accd
Selvan Ramsamy <selvanramsamy@Selvans-MacBook-Pro-2.local>
2022-04-07 08:06:49 +0800

    change headear signature

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from fastapi import FastAPI, Header, Request
+from fastapi import FastAPI, Request, Header
 import stripe
 import os
 import uvicorn
+from pyairtable import Table
 
 
 app = FastAPI()
 
 stripe.api_key = os.environ.get("STRIPE_API_KEY")
-endpoint_secret = os.environ.get("ENDPOINT_SECRET")
 
 @app.post("/webhook")
-async def webhook(request: Request, stripe_signature:str = Header(None)):
-    event = None
+async def webhook(request: Request, stripe_signature: str = Header(None)):
     data = await request.body()
-
+    endpoint_secret = os.environ.get("ENDPOINT_SECRET")
+    print(data)
+    print(stripe_signature)
     try:
-        event = stripe.Webhook.construct_event(
-            payload = data, 
-            sig_header = stripe_signature, 
-            secret = endpoint_secret
-        )
+        event = stripe.Webhook.construct_event(payload=data, sig_header=stripe_signature, secret=endpoint_secret)
+        event_data = event['data']
     except Exception as e:
+        print(e)
         return{"error": str(e)}
 
     if event['type'] == 'payment_intent.succeeded':
       print(event['data']['object'])
+      print('success')
     else:
       print('Unhandled event type {}'.format(event['type']))
 
