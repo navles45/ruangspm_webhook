07cfe9683f71a184447510c07955c6584d1119f2
navles45 <41605172+navles45@users.noreply.github.com>
2022-04-19 14:11:02 +0800

    add a model for revision

--- app/routers/revision.py
+++ app/routers/revision.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from dotenv import load_dotenv
 import openai
 import os
@@ -7,18 +8,22 @@ router = APIRouter()
 load_dotenv()
 openai.api_key = os.environ.get("OPEN_AI")
 
+class Question(BaseModel):
+    query:str
+
 @router.post('/revision/question')
-def query(query:str):
+async def question(query:Question):
+    question_request = query.query
     answers = openai.Answer.create(
         search_model="ada",
         model="curie",
-        question=query,
+        question=question_request,
         documents=["Kedaulatan ditakrifkan sebagai kekuasaan tertinggi dan kewibawaan sesebuah negara yang bebas serta mempunyai hak untuk melaksanakan pemerintahan dan pentadbiran negara."],
         examples_context= "In 2017, U.S. life expectancy was 78.6 years.",
         examples=[["What is human life expectancy in the United States?","78 years."]],
         max_tokens = 100,
         stop= ["\n", "<|endoftext|>"]
     )
-    return answers["answers"]
+    return answers
 
 
